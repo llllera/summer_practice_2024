aa35e4a2df7a4017c2578c1cdf08495a18ce775a
valeria <pechkanovalera@gmail.com>
2024-06-03 16:53:58 +0300

    rr

--- index.php
+++ index.php
@@ -30,6 +30,7 @@
         <tr class="item_row">
             <td><?php echo $pp['name']; ?></td>
             <td><?php echo $pp['phone']; ?></td>
+            <td><?php echo $pp['sport']; ?></td>
             <td>
                 <form action="" method="post">
                     <button class="btn edit-btn" name="change" type="submit">Редактировать</button>
@@ -42,40 +43,97 @@
     </tbody>
        
     </table>
-    <button onclick="showBlock(2)">Список спортменов</button>
+    <button onclick="showBlock(2)">Виды спорта</button>
 
     <table  id="block2" class="content" id="films">
-        <thead>
+    <thead>
             <tr>
-                <th>Название</th>
-                <th>Ингредиенты</th>
-                <th>Действия</th>
+                <th></th>
+                <th class="nullCell"></th>
+                <th class="nullCell"></th>
             </tr>
         </thead>
+        <tbody>
+        <?php $people=selectAll('types_of_sports');
+        foreach ($people as $pp) : ?>
+        <tr class="item_row">
+            <td><?php echo $pp['name']; ?></td>
+            <td>
+                <form action="" method="post">
+                    <button class="btn edit-btn" name="change" type="submit">Редактировать</button>
+                    <button class="btn delete-btn" name="delete" type="submit">Удалить</button>
+                    <input name="id" value="<?php echo $pp['id']; ?>" type="hidden" />
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+       
         
     </table>
-    <button onclick="showBlock(3)">Список спортменов</button>
+    <button onclick="showBlock(3)">Список стадионов</button>
 
     <table  id="block3" class="content" id="films">
-        <thead>
+    <thead>
             <tr>
-                <th>Название</th>
-                <th>Ингредиенты</th>
-                <th>Действия</th>
+                <th></th>
+                <th class="nullCell"></th>
+                <th class="nullCell"></th>
             </tr>
         </thead>
+        <tbody>
+        <?php $people=selectAll('stadions');
+        foreach ($people as $pp) : ?>
+        <tr class="item_row">
+            <td><?php echo $pp['name']; ?></td>
+            <td>
+                <form action="" method="post">
+                    <button class="btn edit-btn" name="change" type="submit">Редактировать</button>
+                    <button class="btn delete-btn" name="delete" type="submit">Удалить</button>
+                    <input name="id" value="<?php echo $pp['id']; ?>" type="hidden" />
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
         
     </table>
-    <button onclick="showBlock(4)">Список спортменов</button>
+    <button onclick="showBlock(4)">Журнал учета выступлений</button>
 
     <table  id="block4" class="content" id="films">
-        <thead>
+    <thead>
             <tr>
-                <th>Название</th>
-                <th>Ингредиенты</th>
-                <th>Действия</th>
+                <th>ФИО</th>
+                <th>Дата</th>
+                <th>Стадион</th>
+                <th class="nullCell"></th>
+                <th class="nullCell"></th>
             </tr>
         </thead>
+        <tbody>
+        <?php 
+            include('data.php');
+            $sth = $db->prepare("SELECT performances.id, performances.date, performances.place, GROUP_CONCAT(sportsmen.name SEPARATOR ', ') AS names
+                                FROM performances
+                                JOIN performances_members ON performances.id = performances_members.id_performance join sportsmen on performances_members.id_member = sportsmen.id
+                                GROUP BY performances.id;");
+            $sth->execute();
+            $people = $sth->fetchAll();
+        foreach ($people as $pp) : ?>
+        <tr class="item_row">
+            <td><?php echo $pp['names']; ?></td>
+            <td><?php echo $pp['date']; ?></td>
+            <td><?php echo $pp['place']; ?></td>
+            <td>
+                <form action="" method="post">
+                    <button class="btn edit-btn" name="change" type="submit">Редактировать</button>
+                    <button class="btn delete-btn" name="delete" type="submit">Удалить</button>
+                    <input name="id" value="<?php echo $pp['id']; ?>" type="hidden" />
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
         
     </table>
 
